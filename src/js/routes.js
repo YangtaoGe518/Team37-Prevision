44c1f357ba434e4337100129c8bb1872b509328e
Ruairidh Williamson <williamsonruairidh@gmail.com>
2019-11-03 23:10:25 +0000

    Fixed login structure and added search bars

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,15 +1,10 @@
 import HomePage from '../pages/home.f7.html';
-import LoginPage from '../pages/login.f7.html';
 import TranslatePage from '../pages/translate.f7.html';
 import AccountPage from '../pages/account.f7.html';
 import FlashcardPage from '../pages/flashcard.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
-  {
-    path: '/',
-    component: LoginPage,
-  },
   {
     path: '/home/',
     component: HomePage,
