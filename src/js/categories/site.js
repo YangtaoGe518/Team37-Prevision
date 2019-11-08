c71aa7b77cc51823d036a301c9117dd7fe385ddd
Yangtao Ge <geyangtao@yahoo.com>
2019-11-08 19:54:54 +0000

    Site page Sketch

--- src/js/categories/site.js
+++ src/js/categories/site.js
@@ -1,6 +1,6 @@
 const videos = [
     {
-        name: 'French Table Manner',
+        name: 'French Travel Advice',
         image: 'https://cdn.framework7.io/placeholder/people-1000x600-6.jpg',
     },
 ].map(el => ({type: 'video', ...el}));
