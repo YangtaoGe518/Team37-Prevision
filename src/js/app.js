6e4b3553df40d964eece5f54875e729632a12444
Yangtao Ge <geyangtao@yahoo.com>
2019-11-10 16:19:04 +0000

    Add FlashCard App

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import $$ from 'dom7';
+import $ from 'jquery';
 import Framework7 from 'framework7/framework7.esm.bundle.js';
 
 // Import F7 Styles
@@ -110,3 +111,71 @@ $$('.open-vertical-target').on('click', function () {
 $$('.reload').on('click', function () {
   window.location.reload();
 });
+
+// FlashcardApp
+$$(document).on('page:init', '.page[data-name="flashcard"]', function () {
+  loadDB();
+})
+
+var colorArray=["#019875","#1E8BC3","#D91E18","#D35400","#8E44AD","#C0392B"];
+var cardState;
+var currentQuestion=0;
+var qbank=new Array;
+
+
+function loadDB(){
+ $.getJSON("./static/json/frenchFood.json", function(data) {
+  for(var i=0;i<data.questionlist.length;i++){
+   qbank[i]=[];
+   qbank[i][0]=data.questionlist[i].cardfront;
+   qbank[i][1]=data.questionlist[i].cardback;
+  }//for
+  beginActivity();
+ })//gtjson
+}//loadDB
+
+function beginActivity(){
+ cardState=0;
+ var color1=colorArray[Math.floor(Math.random()*colorArray.length)];
+
+ $("#card-area").empty();
+ $("#card-area").append('<div id="card1" class="card">' + qbank[currentQuestion][0] + '</div>');
+ $("#card-area").append('<div id="card2" class="card">' + qbank[currentQuestion][1] + '</div>');
+ $("#card1").css("background-color",color1);
+ $("#card2").css("background-color","#34495E");
+ $("#card2").css("top","400px");
+
+ $("#card-area").on("click",function(){
+  if(cardState!=1){
+   cardState=1;
+   //togglePosition();
+   $("#card1").animate({top: "-=200"}, 150, function() {cardState=0;togglePosition();});
+   $("#card2").animate({top: "-=200"}, 150, function() {togglePosition2();});
+  }//if
+ });//click function
+ 
+ currentQuestion++;
+
+ $("#button-area").empty();
+ $("#button-area").append('<p class="row"><button id="skip-button" class="col button button-fill color-red">Study Again</button><button id="next-button" class="col button button-fill color-green">Got it</button></p>');
+
+ $$("#next-button").on("click", function(){
+  if(currentQuestion<qbank.length){beginActivity();}
+  else{displayFinalMessage();}
+ });//click function
+}//beginactivity
+
+function togglePosition(){
+ if($("#card1").position().top==-200){$("#card1").css("top","400px");};
+}//toggle
+
+function togglePosition2(){
+ if($("#card2").position().top==-200){$("#card2").css("top","400px");};
+}//toggle2
+
+function displayFinalMessage(){
+ $("#button-area").empty();
+ $("#card-area").empty();
+ $("#card-area").append('<div id="finalMessage">You have finished the activity.</div>');
+}//final message
+
